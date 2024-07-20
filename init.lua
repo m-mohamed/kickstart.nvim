52457b241d8eac31e007952e622784e619740643
m-mohamed <moabdulmohamed@gmail.com>
2024-07-20 14:36:21 -0700

    Inital commit

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -157,9 +157,18 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
+-- Set highlight on search, but clear on pressing <jk> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', 'jk', '<cmd>nohlsearch<CR>')
+
+--  This is useful for when you want to exit insert mode, without having to press <Esc> twice
+vim.keymap.set('i', 'jk', '<Esc>')
+
+-- This is usefule for when you want to exit visual mode, without having to press <Esc> twice
+vim.keymap.set('v', 'jk', '<Esc>')
+
+-- Obsidian requires this option to be set to 1 or 2
+vim.o.conceallevel = 1
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -176,10 +185,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -566,7 +575,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -574,7 +583,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
@@ -834,7 +843,23 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'python',
+        'typescript',
+        'javascript',
+        'diff',
+        'html',
+        'css',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -872,19 +897,19 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,205 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  -- Smart Splits
+  {
+    'mrjones2014/smart-splits.nvim',
+    opts = {},
+    config = function()
+      local smart_splits = require 'smart-splits'
+
+      -- Key mappings for resizing splits
+      local resize_mappings = {
+        ['<A-h>'] = smart_splits.resize_left,
+        ['<A-j>'] = smart_splits.resize_down,
+        ['<A-k>'] = smart_splits.resize_up,
+        ['<A-l>'] = smart_splits.resize_right,
+      }
+
+      -- Key mappings for moving between splits
+      local move_mappings = {
+        ['<C-h>'] = smart_splits.move_cursor_left,
+        ['<C-j>'] = smart_splits.move_cursor_down,
+        ['<C-k>'] = smart_splits.move_cursor_up,
+        ['<C-l>'] = smart_splits.move_cursor_right,
+      }
+
+      -- Set key mappings
+      for key, func in pairs(resize_mappings) do
+        vim.keymap.set('n', key, func)
+      end
+
+      for key, func in pairs(move_mappings) do
+        vim.keymap.set('n', key, func)
+      end
+    end,
+  },
+
+  -- Obsidian configuration
+  {
+    'epwalsh/obsidian.nvim',
+    opts = {
+      version = '*', -- recommended, use latest release instead of latest commit
+      lazy = true,
+      ft = 'markdown',
+      dependencies = {
+        'nvim-lua/plenary.nvim', -- Required
+        'hrsh7th/nvim-cmp',
+        'nvim-telescope/telescope.nvim',
+        'nvim-treesitter',
+        'epwalsh/pomo.nvim',
+      },
+      workspaces = {
+        {
+          name = 'Slipbox',
+          path = '~/obsidian/Slipbox',
+        },
+      },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      new_notes_location = 'current_dir',
+      prepend_note_id = true,
+      daily_notes = {
+        folder = '5. Dailies',
+        date_format = '%Y-%m-%d',
+        alias_format = '%B %-d, %Y',
+        default_tags = { 'daily-notes' },
+        template = nil, -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      },
+      mappings = {
+        ['<leader>of'] = {
+          action = function()
+            return require('obsidian').util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ['<leader>od'] = {
+          action = function()
+            return require('obsidian').util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+      note_frontmatter_func = function(note)
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = '', project = '' }
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+      templates = {
+        subdir = 'Templates',
+        date_format = '%Y-%m-%d-%a',
+        time_format = '%H:%M',
+        tags = '',
+      },
+    },
+    config = function(_, opts)
+      require('obsidian').setup(opts)
+    end,
+  },
+
+  -- Pomo configuration
+  {
+    'epwalsh/pomo.nvim',
+    opts = {
+      version = '*', -- Recommended, use latest release instead of latest commit
+      lazy = true,
+      cmd = { 'TimerStart', 'TimerRepeat' },
+      dependencies = {
+        'rcarriga/nvim-notify', -- Optional, but highly recommended if you want to use the "Default" timer
+      },
+      notifiers = {
+        {
+          name = 'Default',
+          opts = {
+            sticky = true,
+            title_icon = '󱎫',
+            text_icon = '󰄉',
+          },
+        },
+        {
+          name = 'System',
+          opts = { sound = true }, -- Available on MacOS and Windows natively and on Linux via the libnotify-bin package
+        },
+      },
+    },
+    config = function(_, opts)
+      require('pomo').setup(opts)
+    end,
+  },
+
+  -- Parrot
+  {
+    'frankroeder/parrot.nvim',
+    tag = 'v0.3.7',
+    dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim', 'rcarriga/nvim-notify' },
+    config = function()
+      require('parrot').setup {
+        -- Providers must be explicitly added to make them available.
+        providers = {
+          openai = {
+            api_key = os.getenv 'OPENAI_API_KEY',
+          },
+          anthropic = {
+            api_key = os.getenv 'ANTHROPIC_API_KEY',
+          },
+          gemini = {
+            api_key = os.getenv 'GEMINI_API_KEY',
+          },
+        },
+      }
+    end,
+  },
+  -- Oil
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        columns = { 'icon' },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<M-h>'] = 'actions.select_split',
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Open parent directory in floating window
+      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+    end,
+  },
+
+  -- LazyGit
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+    config = function()
+      require('telescope').load_extension 'lazygit'
+    end,
+  },
+}
